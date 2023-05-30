bc699249000f3ed78bf79b9cbbf375d9e87306b1
Gustavo Ferrara <73325370+gustavoferrara@users.noreply.github.com>
2023-05-29 21:41:38 -0300

    Add technologies section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,144 @@
 import styles from '../styles/landingPage.module.scss';
 
 const LandingPage: React.FC = () => {
+  type ITechnologiesList = Array<{
+    technologyName: string;
+    imageLink: string;
+    type: 'frontend' | 'backend' | 'design';
+  }>;
+
+  const technologiesList: ITechnologiesList = [
+    {
+      technologyName: 'HTML 5',
+      imageLink: '/technologies/HTML.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'CSS 3',
+      imageLink: '/technologies/CSS.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'Javascript',
+      imageLink: '/technologies/js.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'Typescript',
+      imageLink: '/technologies/ts.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'React',
+      imageLink: '/technologies/react.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'Next.JS',
+      imageLink: '/technologies/next.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'Sass',
+      imageLink: '/technologies/sass.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'Post CSS',
+      imageLink: '/technologies/postcss.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'Git',
+      imageLink: '/technologies/git.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'Webpack',
+      imageLink: '/technologies/webpack.svg',
+      type: 'frontend',
+    },
+    {
+      technologyName: 'NodeJS - Express',
+      imageLink: '/technologies/nodeexpress.svg',
+      type: 'backend',
+    },
+    {
+      technologyName: 'MongoDB',
+      imageLink: '/technologies/mongodb.svg',
+      type: 'backend',
+    },
+    {
+      technologyName: 'Nginx',
+      imageLink: '/technologies/nginx.svg',
+      type: 'backend',
+    },
+    {
+      technologyName: 'Figma',
+      imageLink: '/technologies/figma.svg',
+      type: 'design',
+    },
+    {
+      technologyName: 'Adobe Photoshop',
+      imageLink: '/technologies/photoshop.svg',
+      type: 'design',
+    },
+    {
+      technologyName: 'Adobe Illustrator',
+      imageLink: '/technologies/illustrator.svg',
+      type: 'design',
+    },
+  ];
+
   return (
     <main className={styles.home_wrapper}>
       <section className={styles.hero}>
-        <h1 className={styles.hero_heading}>I&apos;m Gustavo Ferrara</h1>
-        <h2 className={styles.hero_subheading}>Full-stack developer</h2>
+        <h1 className={styles.hero_heading}>
+          I&apos;m Gustavo Ferrara <span>Full-stack developer</span>
+        </h1>
         <img src='/logo.svg' alt='' />
+      </section>
+      <section className={styles.technologies}>
+        <h2 className={styles.technologies_heading}>
+          I work with these technologies
+        </h2>
+        <div className={styles.technologies_grid}>
+          <p>Front-end</p>
+          {technologiesList
+            .filter(item => item.type === 'frontend')
+            .map(item => (
+              <div className={styles.technology_item} key={item.imageLink}>
+                <img src={item.imageLink} alt='' />
+                <p>{item.technologyName}</p>
+              </div>
+            ))}
+        </div>
+        <div className={styles.technologies_grid}>
+          <p>Back-end</p>
+          {technologiesList
+            .filter(item => item.type === 'backend')
+            .map(item => (
+              <div className={styles.technology_item} key={item.imageLink}>
+                <img src={item.imageLink} alt='' />
+                <p>{item.technologyName}</p>
+              </div>
+            ))}
+        </div>
+        <div className={styles.technologies_grid}>
+          <p>Design</p>
+          {technologiesList
+            .filter(item => item.type === 'design')
+            .map(item => (
+              <div className={styles.technology_item} key={item.imageLink}>
+                <img src={item.imageLink} alt='' />
+                <p>{item.technologyName}</p>
+              </div>
+            ))}
+        </div>
+        <p>
+          ...and more! I&apos;m always eager to learn new tools which make me a
+          better professional.
+        </p>
       </section>
     </main>
   );
