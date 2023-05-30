4324cc75badd06a16065fec9c6f0fd2498e52451
Gustavo Ferrara <73325370+gustavoferrara@users.noreply.github.com>
2023-05-29 21:23:13 -0300

    Add hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
+import styles from '../styles/landingPage.module.scss';
+
 const LandingPage: React.FC = () => {
   return (
-    <div>
-      <div>test</div>
-    </div>
+    <main className={styles.home_wrapper}>
+      <section className={styles.hero}>
+        <h1 className={styles.hero_heading}>I&apos;m Gustavo Ferrara</h1>
+        <h2 className={styles.hero_subheading}>Full-stack developer</h2>
+        <img src='/logo.svg' alt='' />
+      </section>
+    </main>
   );
 };
 
