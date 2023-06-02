8e2d23fda63ec5b4dc44233bed916c79dc5dbdd9
Gustavo Ferrara <73325370+gustavoferrara@users.noreply.github.com>
2023-06-02 15:06:32 -0300

    Add home button functionality on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,31 @@
+import { useRouter } from 'next/router';
+
 import styles from '@/styles/navbar.module.scss';
 
 const Navbar = () => {
+  const router = useRouter();
+
   // const toggleMobileNavbar = () => {};
+
+  const handleHomeButtonClick = () => {
+    if (router.pathname === '/') {
+      const heroSection = document.querySelector('.landingPage_hero__yOqZh')!;
+
+      heroSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    } else router.push('/');
+  };
 
   return (
     <header id={styles.header}>
       <nav id={styles.navbar}>
-        <button className={styles.navbar_homebtn}>
+        <button
+          onClick={handleHomeButtonClick}
+          className={styles.navbar_homebtn}
+        >
           <img src='/logo.svg' alt='' />
         </button>
         <button className={styles.navbar_hamburgerbtn}>
